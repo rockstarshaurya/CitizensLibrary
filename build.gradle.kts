1a23560d9ee8e732113f148f3abe7d8a97d572c5
rockstarshaurya <73274505+rockstarshaurya@users.noreply.github.com>
2025-06-26 19:46:13 +0530

    Fix: Correct folder structure for JitPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,19 @@
-plugins {}
+plugins {
+    `maven-publish`
+}
+
+group = "citizens.library"
+version = "1.0.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            artifact("maven/citizens/library/1.0.0/citizenslibrary-1.0.0.aar") {
+                extension = "aar"
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
